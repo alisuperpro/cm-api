c63be10aabbf49a6e9ba53c0f5295c2d4dbe6486
alisuperpro <alisolorzanove@gmail.com>
2026-03-12 14:15:35 -0400

    add rate limit

--- src/api/index.ts
+++ src/api/index.ts
@@ -8,6 +8,7 @@ import { setupEmailService } from '../events/email.services'
 import { setupAdminUserService } from '../events/adminUser.event'
 import { paysUploadDir } from '../routes/trainingUser.routes'
 import { videoUploadDir } from '../routes/video.routes'
+import { rateLimit } from 'express-rate-limit'
 
 const app = express()
 dotenv.config()
@@ -24,6 +25,16 @@ app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(clerkMiddleware())
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+})
+
+app.use(limiter)
+
 setupEmailService()
 setupAdminUserService()
 
