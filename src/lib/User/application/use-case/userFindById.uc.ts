0901dc342e005967b465d1f59207b5385b4ef1c3
alisuperpro <alisolorzanove@gmail.com>
2026-04-10 19:41:19 -0400

    using userFindByIdDTO

--- src/lib/User/application/use-case/userFindById.uc.ts
+++ src/lib/User/application/use-case/userFindById.uc.ts
@@ -1,12 +1,13 @@
 import { UserNotFoundError } from '../../domain/errors/userNotFoundError.error'
 import { UserRepository } from '../../domain/repository/user.repository'
 import { UserId } from '../../domain/value-objects/userId.vo'
+import { UserFindByIdDTO } from '../dto/userFindById.dto'
 
 export class UserFindById {
     constructor(private repository: UserRepository) {}
 
-    async run(id: string) {
-        const user = await this.repository.findById(new UserId(id))
+    async run(dto: UserFindByIdDTO) {
+        const user = await this.repository.findById(new UserId(dto.id))
 
         if (!user) throw new UserNotFoundError('User not found')
 
