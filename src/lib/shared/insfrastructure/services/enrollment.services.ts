555e919eccd0db775f52d14893b900cfc97e1f1d
alisuperpro <alisolorzanove@gmail.com>
2026-05-02 16:14:16 -0400

    feat: add enrollment services and integrate into service container

---
+++ src/lib/shared/insfrastructure/services/enrollment.services.ts
@@ -0,0 +1,24 @@
+import { EnrollmentCreate } from '@/lib/Enrollment/application/use-cases/enrollmentCreate.uc'
+import { EnrollmentGetAll } from '@/lib/Enrollment/application/use-cases/enrollmentGetAll.uc'
+import { EnrollmentGetById } from '@/lib/Enrollment/application/use-cases/enrollmentGetById.uc'
+import { EnrollmentGetByTraining } from '@/lib/Enrollment/application/use-cases/enrollmentGetByTraining.uc'
+import { EnrollmentUpdateCertificateReceived } from '@/lib/Enrollment/application/use-cases/enrollmentUpdateCertificateReceived.uc'
+import { EnrollmentUpdateIsArrived } from '@/lib/Enrollment/application/use-cases/enrollmentUpdateIsArrived.uc'
+import { EnrollmentUpdatePayConfirmed } from '@/lib/Enrollment/application/use-cases/enrollmentUpdatePayConfirmed.uc'
+import { EnrollmentQueryRepositoryImpl } from '@/lib/Enrollment/infrastructure/query/enrollmentQuery.repository'
+import { EnrollmentQueryTursoRepository } from '@/lib/Enrollment/infrastructure/repository/enrollment.repository'
+
+const enrollmentRepository = new EnrollmentQueryTursoRepository()
+const enrollmentQueryRepository = new EnrollmentQueryRepositoryImpl()
+
+export const enrollmentServices = {
+    create: new EnrollmentCreate(enrollmentRepository),
+    getByTraining: new EnrollmentGetByTraining(enrollmentQueryRepository),
+    getAll: new EnrollmentGetAll(enrollmentQueryRepository),
+    getById: new EnrollmentGetById(enrollmentQueryRepository),
+    updateIsArrived: new EnrollmentUpdateIsArrived(enrollmentRepository),
+    updatePayConfirmed: new EnrollmentUpdatePayConfirmed(enrollmentRepository),
+    updateCertificateReceived: new EnrollmentUpdateCertificateReceived(
+        enrollmentRepository
+    ),
+}
--- src/lib/shared/insfrastructure/services/serviceContainer.ts
+++ src/lib/shared/insfrastructure/services/serviceContainer.ts
@@ -5,24 +5,14 @@ import { EmailTemplateCreate } from '@/lib/Email/application/use-case/emailTempl
 import { EmailTemplateGetAll } from '@/lib/Email/application/use-case/emailTemplateGetAll.uc'
 import { EmailSystemTursoRepository } from '@/lib/Email/infrastructure/repository/emailSystem.repository'
 import { EmailTemplateTursoRepository } from '@/lib/Email/infrastructure/repository/emailTemplate.repository'
-import { EnrollmentCreate } from '@/lib/Enrollment/application/use-cases/enrollmentCreate.uc'
-import { EnrollmentGetAll } from '@/lib/Enrollment/application/use-cases/enrollmentGetAll.uc'
-import { EnrollmentGetById } from '@/lib/Enrollment/application/use-cases/enrollmentGetById.uc'
-import { EnrollmentGetByTraining } from '@/lib/Enrollment/application/use-cases/enrollmentGetByTraining.uc'
-import { EnrollmentUpdateCertificateReceived } from '@/lib/Enrollment/application/use-cases/enrollmentUpdateCertificateReceived.uc'
-import { EnrollmentUpdateIsArrived } from '@/lib/Enrollment/application/use-cases/enrollmentUpdateIsArrived.uc'
-import { EnrollmentUpdatePayConfirmed } from '@/lib/Enrollment/application/use-cases/enrollmentUpdatePayConfirmed.uc'
-import { EnrollmentQueryRepositoryImpl } from '@/lib/Enrollment/infrastructure/query/enrollmentQuery.repository'
-import { EnrollmentQueryTursoRepository } from '@/lib/Enrollment/infrastructure/repository/enrollment.repository'
 
 import { userServices } from './user.services'
 import { trainingServices } from './training.services'
 import { adminUserServices } from './adminUser.services'
 import { trainingStatusServices } from './trainingStatus.services'
 import { trainingTypeServices } from './trainingType.services'
+import { enrollmentServices } from './enrollment.services'
 
-const enrollmentRepository = new EnrollmentQueryTursoRepository()
-const enrollmentQueryRepository = new EnrollmentQueryRepositoryImpl()
 const emailSystemRepository = new EmailSystemTursoRepository()
 const emailTemplateRepository = new EmailTemplateTursoRepository()
 
@@ -43,17 +33,7 @@ export const serviceContainer = {
         ...trainingTypeServices,
     },
     enrollment: {
-        create: new EnrollmentCreate(enrollmentRepository),
-        getByTraining: new EnrollmentGetByTraining(enrollmentQueryRepository),
-        getAll: new EnrollmentGetAll(enrollmentQueryRepository),
-        getById: new EnrollmentGetById(enrollmentQueryRepository),
-        updateIsArrived: new EnrollmentUpdateIsArrived(enrollmentRepository),
-        updatePayConfirmed: new EnrollmentUpdatePayConfirmed(
-            enrollmentRepository
-        ),
-        updateCertificateReceived: new EnrollmentUpdateCertificateReceived(
-            enrollmentRepository
-        ),
+        ...enrollmentServices,
     },
     emailSystem: {
         create: new EmailSystemCreate(emailSystemRepository),
