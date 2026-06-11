44f7b8098e1aea60488d847b7008d096787980ff
alisuperpro <alisolorzanove@gmail.com>
2026-06-11 18:40:56 -0400

    Refactor employeeContactRouter to include middleware for admin authentication and ID validation

--- src/lib/EmployeeContact/infrastructure/http/express/routes/employeeContact.routes.ts
+++ src/lib/EmployeeContact/infrastructure/http/express/routes/employeeContact.routes.ts
@@ -1,13 +1,20 @@
 import { Router } from 'express'
 import { EmployeeContactController } from '../controller/employeeContact.controller'
+import { checkAdminAuth } from '@/lib/shared/insfrastructure/http/middleware/checkAdminAuth.middleware'
+import { checkId } from '@/lib/shared/insfrastructure/http/middleware/checkId.middleware'
 
 export const employeeContactRouter = Router()
 
 const employeeContactController = new EmployeeContactController()
 
-employeeContactRouter.post('/', employeeContactController.create)
+employeeContactRouter.post(
+    '/',
+    checkAdminAuth,
+    employeeContactController.create
+)
 
 employeeContactRouter.get(
     '/employee/:id',
+    checkId,
     employeeContactController.findByEmployeeId
 )
