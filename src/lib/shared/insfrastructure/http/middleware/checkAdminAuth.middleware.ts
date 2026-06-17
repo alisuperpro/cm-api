cadb633cc1b722a08c2803f722cfd92c372d9871
alisuperpro <alisolorzanove@gmail.com>
2026-06-16 20:07:34 -0400

    Refactor checkAdminAuth middleware to integrate Sentry error handling and improve authentication error responses

--- src/lib/shared/insfrastructure/http/middleware/checkAdminAuth.middleware.ts
+++ src/lib/shared/insfrastructure/http/middleware/checkAdminAuth.middleware.ts
@@ -1,25 +1,34 @@
 import { clerkClient, getAuth } from '@clerk/express'
 import { NextFunction, Request, Response } from 'express'
+import { SentryErrorHandler } from '../../monitoring/sentryHandler'
+import { ErrorHandler } from '@/lib/shared/domain/repository/error.repository'
 
-export const checkAdminAuth = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-) => {
-    console.log('checking if session exists')
-    const { isAuthenticated, userId } = getAuth(req)
+const errorHandler = new SentryErrorHandler()
 
-    if (!isAuthenticated) {
-        return res.status(401).json({ error: 'User not authenticated' })
+const createCheckAdminAuthMiddleware = (errorHandler: ErrorHandler) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        const { isAuthenticated, userId } = getAuth(req)
+
+        if (!isAuthenticated) {
+            errorHandler.captureException(
+                new Error('Employee not authenticated'),
+                {
+                    path: req.originalUrl,
+                }
+            )
+            return res.status(401).json({ error: 'Employee not authenticated' })
+        }
+
+        const user = await clerkClient.users.getUser(userId)
+
+        if (user.publicMetadata.role === 'Admin') {
+            //@ts-ignore
+            req.user = { ...user }
+        } else {
+            return res.status(401).json({ error: 'User not employee' })
+        }
+        next()
     }
-
-    const user = await clerkClient.users.getUser(userId)
-
-    if (user.publicMetadata.role === 'Admin') {
-        //@ts-ignore
-        req.user = { ...user }
-    } else {
-        return res.status(401).json({ error: 'User not authenticated' })
-    }
-    next()
 }
+
+export const checkAdminAuth = createCheckAdminAuthMiddleware(errorHandler)
